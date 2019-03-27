708502ddc3b604b4a81205e5231b287130b9d46d
waichong <wctan4444@yahoo.com>
2019-03-27 22:10:02 +0800

    Using Java Configuration - @Configuration and @Bean

---
+++ src/main/java/twc/springframework/joke/config/ChunkConfiguration.java
@@ -0,0 +1,15 @@
+package twc.springframework.joke.config;
+
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChunkConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes(){
+        return new ChuckNorrisQuotes();
+    }
+}
--- src/main/java/twc/springframework/joke/services/JokeServiceImpl.java
+++ src/main/java/twc/springframework/joke/services/JokeServiceImpl.java
@@ -8,12 +8,18 @@ public class JokeServiceImpl implements JokeService {
 
     private final ChuckNorrisQuotes chuckNorrisQuotes;
 
-    public JokeServiceImpl() {
-        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
     }
+
+    //public JokeServiceImpl() {
+    //
+    //    this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    //}
 
     @Override
     public String getJoke() {
+
         return chuckNorrisQuotes.getRandomQuote();
     }
 }
